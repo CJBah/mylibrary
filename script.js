02f16872c98c9a0f9a55b011d632e8e1e11a86c5
RufoH <rufoh98@gmail.com>
2021-12-18 12:20:56 +0100

    Finalized style and script

--- script.js
+++ script.js
@@ -8,11 +8,27 @@ function Book(title, author, pages, read){
 }
 
 function addBookToLibrary(){
-    let newBookTitle = prompt("TITOLONE?","TITOLONE")
-    let newBookAuthor = prompt("AUTORONE?","AUTORONE")
-    let newBookPages = prompt("PAGINONE?","PAGINONE")
+    let newBookTitle = bookForm.title.value;
+    let newBookAuthor = bookForm.author.value;
+    let newBookPages = bookForm.pages.value;
     let newBookReadStatus = true; 
     myLibrary.push(new Book(newBookTitle, newBookAuthor, newBookPages, newBookReadStatus));    
+}
+
+function openEditForm(){
+    document.querySelector("#editFormDiv").style.display = "block";
+}
+
+function clearValues(){
+    bookForm.title.value = "";
+    bookForm.author.value = "";
+    bookForm.pages.value = "";
+}
+
+function clearValuesEdit(){
+    editForm.title.value = "";
+    editForm.author.value = "";
+    editForm.pages.value = "";
 }
 
 function displayBook() {
@@ -21,29 +37,36 @@ function displayBook() {
         let newBookPar1 = document.createElement("p");
         newBookPar1.textContent = myLibrary[i].title;
         let newBookPar2 = document.createElement("p");
-        newBookPar2.textContent = myLibrary[i].author;
+        newBookPar2.textContent = "by " + myLibrary[i].author;
         let newBookPar3 = document.createElement("p");
-        newBookPar3.textContent = myLibrary[i].pages;
+        newBookPar3.textContent = "Pages: " + myLibrary[i].pages;
         newBookDiv.appendChild(newBookPar1);
         newBookDiv.appendChild(newBookPar2);
         newBookDiv.appendChild(newBookPar3);
 
-        let updateBookButton = document.createElement("button");
+        let updateBookButton = document.createElement("button"); 
         updateBookButton.setAttribute("id","updateBookButton");
         updateBookButton.setAttribute("data", i);
-        updateBookButton.textContent = "edit";
+        updateBookButton.textContent = "✏️";
         updateBookButton.onclick = function(){
-            myLibrary[i].title = prompt("Nuovo titolo?",myLibrary[i].title)
-            myLibrary[i].author = prompt("Nuovo autore?",myLibrary[i].author)
-            myLibrary[i].pages = prompt("Nuove pagine?",myLibrary[i].pages)
+            openEditForm()
+        }
+        
+        let closeEditButton = document.querySelector("#closeEditForm")
+        closeEditButton.onclick = function(){
+            myLibrary[i].title = editForm.title.value;
+            myLibrary[i].author = editForm.author.value;
+            myLibrary[i].pages = editForm.pages.value;
+            document.querySelector("#editFormDiv").style.display = "none";
             clearDiv();
             displayBook();
+            clearValuesEdit();
         }
 
         let deleteBookButton = document.createElement("button");
         deleteBookButton.setAttribute("id","deleteBookButton");
         deleteBookButton.setAttribute("data", i);
-        deleteBookButton.textContent = "delete";
+        deleteBookButton.textContent = "🗑️";
         deleteBookButton.onclick = function(){
             myLibrary.splice(i,1);
             clearDiv();
@@ -68,24 +91,28 @@ function displayBook() {
             }
         }
 
-        displayBookDiv.appendChild(updateBookButton);
-        displayBookDiv.appendChild(deleteBookButton);
-        displayBookDiv.appendChild(readStatusButton);
+        newBookDiv.appendChild(updateBookButton);
+        newBookDiv.appendChild(deleteBookButton);
+        newBookDiv.appendChild(readStatusButton);
+        newBookDiv.classList.add("singleBookDiv");
+        
         displayBookDiv.appendChild(newBookDiv);
 
     }
 }
 
 function clearDiv () {
-    displayBookDiv.innerHTML = " "
+    displayBookDiv.innerHTML = ""
 }
 
 const addBookButton = document.querySelector("#addBook");
 const displayBookDiv = document.querySelector("#displayBook");
+const bookForm = document.querySelector(".form-container");
+const editForm = document.querySelector("#editForm");
 
 addBookButton.onclick = function(){
     addBookToLibrary();
     clearDiv();
     displayBook();
+    clearValues();
 }
-
